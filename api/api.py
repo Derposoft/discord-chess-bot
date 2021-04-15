55053cb3b4308f6cec0f6a9e54078cca48e9178c
derposoft <viswanath.chadalapaka@gmail.com>
2021-04-15 14:08:39 -0700

    +started -new

--- api/api.py
+++ api/api.py
@@ -1,10 +1,12 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, url_for, request
+import utils
 app = Flask(__name__)
 
 @app.route('/new')
 def new(name):
     # create a new game for the player
-   return 'welcome %s' % name
+    args = utils.parse(request)
+    return 'New game successfully started for %s' % args["name"]
 
 @app.route('/move',methods = ['POST', 'GET'])
 def move():
@@ -22,7 +24,6 @@ def ff():
 def cheat():
     # allow the player to cheat
     return 'under construction'
-
 
 if __name__ == '__main__':
    app.run(debug = True)
