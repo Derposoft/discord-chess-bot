824e6911dcdbc600bd1ea4eee090b32c290db8df
derposoft <viswanath.chadalapaka@gmail.com>
2021-04-15 13:56:57 -0700

    +matched api, updated reqs for flask/stockfish

--- api/api.py
+++ api/api.py
@@ -1,18 +1,28 @@
 from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
-@app.route('/success/<name>')
-def success(name):
+@app.route('/new')
+def new(name):
+    # create a new game for the player
    return 'welcome %s' % name
 
-@app.route('/login',methods = ['POST', 'GET'])
-def login():
-   if request.method == 'POST':
-      user = request.form['nm']
-      return redirect(url_for('success',name = user))
-   else:
-      user = request.args.get('nm')
-      return redirect(url_for('success',name = user))
+@app.route('/move',methods = ['POST', 'GET'])
+def move():
+    # make a move for the player
+    return 'under construction'
+
+
+@app.route('/ff',methods = ['POST', 'GET'])
+def ff():
+    # accept the player's resignation
+    return 'under construction'
+
+
+@app.route('/cheat',methods = ['POST', 'GET'])
+def cheat():
+    # allow the player to cheat
+    return 'under construction'
+
 
 if __name__ == '__main__':
    app.run(debug = True)
