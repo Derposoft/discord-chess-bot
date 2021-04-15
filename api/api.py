47044968b68332dab34bdb74aa00dba9901db08a
derposoft <viswanath.chadalapaka@gmail.com>
2021-04-15 13:43:28 -0700

    +basic api commit

--- api/api.py
+++ api/api.py
@@ -1,49 +1,18 @@
-import uvicorn
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from flask import Flask, redirect, url_for, request
+app = Flask(__name__)
 
-from routers import trades_router, quotes_router
-import config
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
 
-def serve():
-    uvicorn.run(
-        'api:api',
-        host=config.http_host,
-        port=config.http_port,
-        reload=True
-    )
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success',name = user))
+   else:
+      user = request.args.get('nm')
+      return redirect(url_for('success',name = user))
 
-api = FastAPI()
-
-# api.include_router(
-#     users.router,
-#     tags=['users'],
-#     prefix='/users'
-# )
-api.include_router(
-    trades_router.router,
-    tags=['trades'],
-    prefix='/trades'
-)
-api.include_router(
-    quotes_router.router,
-    tags=['quotes'],
-    prefix='/quotes'
-)
-
-origins = [
-    'http://localhost:8080',
-    'http://127.0.0.1:8080',
-]
-
-api.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*'],
-)
-
-@api.on_event('startup')
-async def startup():
-    config.load_api_keys()
+if __name__ == '__main__':
+   app.run(debug = True)
