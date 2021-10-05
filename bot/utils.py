d8289da3b45333d8829991a4edcedc1d1979c9b7
derposoft <viswanath.chadalapaka@gmail.com>
2021-10-04 23:18:21 -0700

    +i swear i will start working on this again

--- bot/utils.py
+++ bot/utils.py
@@ -1,3 +1,12 @@
+# returns user info
 def user_info(ctx):
     return 'uid=' + str(ctx.author.id) + '&name=' + ctx.author.name
+
+# parse @mentions
+def mention_parser(ctx):
+    return 0
+
+# UNDER CONSTRUCTION (this one is hard as it requires board context)
+def sfmove_to_algebraic(sfmove):
+    return 0
     
