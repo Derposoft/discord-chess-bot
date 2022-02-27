f71e3c7804f800efcec162bf6a7425128be62750
Tristan Hilbert (aka TFlexSoom) <tchilbert@live.com>
2022-02-26 21:25:37 -0800

    Add Logging to Discord Bot

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
-import json, requests, utils, sys, argparse
+import json, requests, utils, sys, argparse, logging
+import dummylog
 
 ### Get Command Line Arguments and configuration
 description = """Discord Chess Bot is a simple application to play againt other server
@@ -9,9 +10,15 @@ members or to play against the Stockfish AI. Use -h or --help for information ab
 argparser = argparse.ArgumentParser(description=description)
 argparser.add_argument("-f", "--file", "--config", dest="configPath", default="./config.json", help = "File Path to Config File")
 argparser.add_argument("-u", "--host", dest="url", default="http://localhost:5000/", help="url for acessing discord bot rest api")
+argparser.add_argument("-l", "--log", dest="log", default="discord.log", help="output file path for debug logging!")
+argparser.add_argument("--quiet", "--no-log", dest="quiet", default=False, action="store_const", const=True, help="Disable logging from bot. Not recommended!")
 args = argparser.parse_args()
 
-config = {'url': args.url}
+config = {
+    'url': args.url,
+    'log': args.log,
+    'quiet': args.quiet
+    }
 
 print(f"Loading config file from {args.configPath}")
 with open(args.configPath, 'r') as configFile:
@@ -21,9 +28,20 @@ with open(args.configPath, 'r') as configFile:
     utils.safeDictCopy(config, ['password'], data, ['bot', 'password'])
     utils.safeDictCopyDefault(config, ['prefix'], data, ['bot', 'prefix'], '-')
     utils.safeDictCopyDefault(config, ['botDesc'], data, ['bot', 'description'], 'blindfold chess bot')
+    utils.safeDictCopy(config, ['log'], data, ['bot', 'log'])
 
 if 'key' not in config:
     exit(1)
+
+### Initialize Log
+if not config['quiet']:
+    logger = logging.getLogger('discord')
+    logger.setLevel(logging.DEBUG)
+    handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+    logger.addHandler(handler)
+else:
+    logger = dummylog()
 
 ### Initialize bot
 intents = discord.Intents.default()
@@ -41,10 +59,14 @@ async def on_ready():
 async def new(ctx, side: str, eloOrMention: str = '1500'):
     """starts a new chess game VS CPU/human for the current user given an option of side: b, w, or r (random)."""
 
+    logger.debug(f'Game Created By {ctx.author.id} with side {side} and elo {eloOrMention}')
+
     if utils.is_elo(eloOrMention):
+        logger.debug(f'Creating Solo Game!')
         info = requests.get(config['url'] + 'new?side=' + side.lstrip().rstrip() 
             + '&elo=' + eloOrMention + '&' + utils.user_info(ctx))
     else:
+        logger.debug(f'Creating 2 Player Game!')
         info = requests.get(config['url'] + 'new?side=' + side.lstrip().rstrip() 
             + '&challengee=' + utils.mention_parser(eloOrMention) + '&challenger=' + str(ctx.author.id))
     await ctx.send(info.text)
@@ -53,6 +75,7 @@ async def new(ctx, side: str, eloOrMention: str = '1500'):
 async def move(ctx, move: str, player: str = ''):
     """makes a move in a currently running game. add an @mention at end for a pvp game."""
 
+    logger.debug(f'Move made by {ctx.author.id} with move {move} against player \'{player}\'')
     info = requests.get(f'{config["url"]}move?move={move}&{utils.user_info(ctx)}{utils.pvpstring(player)}')
     await ctx.send(info.text)
 
@@ -60,6 +83,7 @@ async def move(ctx, move: str, player: str = ''):
 async def ff(ctx, player: str = ''):
     """resign the currently running game"""
 
+    logger.debug(f'Forfeit yielded by {ctx.author.id} against player \'{player}\'')
     info = requests.get(f'{config["url"]}ff?{utils.user_info(ctx)}{utils.pvpstring(player)}')
     await ctx.send(info.text)
 
@@ -67,8 +91,10 @@ async def ff(ctx, player: str = ''):
 async def cheat(ctx, player: str = ''):
     """allow the user to cheat by providing a boardstate and an evaluation. add an @mention at the end for a pvp game"""
 
+    logger.debug(f'Cheat used by {ctx.author.id} against player \'{player}\'')
     info = requests.get(f'{config["url"]}cheat?{utils.user_info(ctx)}{utils.pvpstring(player)}')
     await ctx.send(info.text)
 
 ### Run the Bot Event Loop (infinite loop)
+logger.debug("Running Bot!")
 bot.run(config['key'])
