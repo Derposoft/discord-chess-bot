fbda218e62049a264a85b27fe68faaae0f7715bb
TFlexSoom <tchilbert@live.com>
2022-02-27 01:55:13 -0800

    Start Working on Bugfixes. Create New Tables for less hacky data.

--- api/api.py
+++ api/api.py
@@ -1,8 +1,9 @@
 from flask import Flask, url_for, request
-from db.database import init, db_session, Game
+from db.database import init, db_session, GameLegacy,\
+    Participant, StockfishGame, StockfishMoves, CompetitiveGame, CompetitiveMoves
 from stockfish import Stockfish
-import argparse, json, logging
-import dummylog, utils
+import argparse, json
+import utils
 
 ### Get Command Line Arguments and configuration
 description = """Chess Move Restful API"""
@@ -10,8 +11,6 @@ description = """Chess Move Restful API"""
 argparser = argparse.ArgumentParser(description=description)
 argparser.add_argument("-f", "--file", "--config", dest="configPath", default="./config.json", help = "File Path to Config File")
 argparser.add_argument("--db", "--database", dest="db", default="sqlite:///storage.db", help="SQL URI for accessing SQL database")
-argparser.add_argument("-l", "--log", dest="log", default="api.log", help="output file path for debug logging!")
-argparser.add_argument("--quiet", "--no-log", dest="quiet", default=False, action="store_const", const=True, help="Disable logging from bot. Not recommended!")
 args = argparser.parse_args()
 
 config = {
@@ -26,174 +25,226 @@ with open(args.configPath, 'r') as configFile:
     utils.safe_dict_copy_default(config, ['host'], data, ['api', 'host'], "0.0.0.0")
     utils.safe_dict_copy_default(config, ['port'], data, ['api', 'port'], 8000)
     utils.safe_dict_copy_default(config, ['stockfish'], data, ['api', 'stockfish'], '/usr/games/stockfish')
-    utils.safe_dict_copy(config, ['log'], data, ['bot', 'log'])
-
-### Initialize Log
-if not config['quiet']:
-    logger = logging.getLogger('discord')
-    logger.setLevel(logging.DEBUG)
-    handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-    logger.addHandler(handler)
-else:
-    logger = dummylog()
 
 init(config['db'])
 stockfish = Stockfish(config['stockfish'])
 
 app = Flask(__name__)
 
+WHITE = 'white'
+BLACK = 'black'
+
+# TODO We should provide HTTP Status codes for this API if we want to support it long-term
+
 # creates a new game for the player
-@app.route('/new')
+@app.route('/new/ai', methods = ['POST'])
+def new_stockfish():
+    args = utils.parse_args(request)
+    db = db_session()
+    side = args['side']
+    author = utils.get_first_valid_index(args, ['author'])
+    elo = utils.get_first_valid_index(args, ['elo'])
+
+    validation = validate_new_game(False, side, author, None)
+    if validation is not None:
+        return validation
+    elif elo is None:
+        return f'No Elo Setting Provided for AI Game!'
+    
+     # check to make sure player doesn't already have a game
+    curr_game = utils.check_in_game(author, db)
+    if curr_game != None:
+        return f'bruh ur in a gam rn i will only ple 1 game with u at a time {utils.mention(args)}'
+    
+    player_is_white = side == WHITE
+    if not player_is_white:
+        # make a move and game to db if player is black and vs CPU
+        move = utils.engine_move(stockfish, '', args['elo'])
+    
+    # TODO author id FK failure
+    game = StockfishGame(partipant_id=author, stockfish_elo=elo, player_is_white=player_is_white)
+    db.add(game)
+    db.commit()
+
+    return f'New game successfully started for {utils.mention(author)}. {utils.relay_move(move, args)}'
+
+
+@app.route('/new/pvp', methods = ['POST'])    
 def new():
     args = utils.parse_args(request)
     db = db_session()
     side = args['side']
-    # TODO challengee is very close to challenger... maybe the parameter should
-    #    be something more distinguished? P1 versus P2? Author vs Invitee?
-    author = utils.get_first_valid_index(args, ['challengee', 'uid'])
-    invitee = utils.get_first_valid_index(args, ['challenger'])
-    elo = utils.get_first_valid_index(args, ['elo'])
+    author = utils.get_first_valid_index(args, ['author'])
+    invitee = utils.get_first_valid_index(args, ['invitee'])
 
-    # check to make sure player only chose black or white
-    if side != 'white' and side != 'black':
-        return 'you can only be \'white\' or \'black\' because this is chess OMEGALUL'
-    elif author is None:
-        return 'A unique ID for the player must be provided!'
+    validation = validate_new_game(True, side, author, invitee)
+    if validation is not None:
+        return validation
 
-    # this is a pvp game
-    game_id = ''
-    is_pvp = 'challenger' in args
-    if is_pvp:
-        logger.debug(author, invitee)
-        curr_game = utils.check_in_pvp(author, invitee, db)
-        if curr_game != None:
-            return f'bruhh you can only ple 1 game with a given person at once {utils.mention_player(author)}'
-        game_id = f'{author},{invitee}'
-
-    # this is a vs CPU game
-    else:
-        # check to make sure player doesn't already have a game
-        curr_game = utils.check_in_game(author, db)
-        if curr_game != None:
-            return f'bruh ur in a gam rn i will only ple 1 game with u at a time {utils.mention(args)}'
-        game_id = args['uid']
-    logger.debug(f'choosing game id {game_id}')
-
-    # check to see which side player is
-    move = ''
-    if side == 'black' and not is_pvp:
-        # make a move and game to db if player is black and vs CPU
-        move = utils.engine_move(stockfish, '', args['elo'])
-
-    # i'm using the stockfish_elo to hold which player's turn it is because i'm lazy. ez hack no judge me pls
-    elo = elo if not is_pvp else args['side'] == 'black'
-    game = Game(uid=game_id, moves=move, stockfish_elo=elo, player_side=args['side'])
+    app.logger.debug(f'Attempting Creation for pvp game {author} and {invitee}')
+    curr_game = utils.check_in_pvp(author, invitee, db)
+    if curr_game != None:
+        return f'bruhh you can only ple 1 game with a given person at once {utils.mention_player(author)}'
+    
+    author_is_white=side == WHITE
+    game = CompetitiveGame(author_id=author, invitee_id=invitee, author_is_white=author_is_white)
     db.add(game)
     db.commit()
 
-    # report back
-    info_string = ''
-    if is_pvp:
-        p1, p2 = args['challenger'], args['challengee']
-        info_string = f'New game successfully started between {utils.mention_player(p1)} and {utils.mention_player(p2)}.'
-    else:
-        info_string = f'New game successfully started for {utils.mention(args)}. {utils.relay_move(move, args)}'
-    return info_string
+    return f'New game successfully started between {utils.mention_player(author)} and {utils.mention_player(invitee)}.'
+
+def validate_new_game(is_pvp, side, author, invitee):
+    if side != 'white' and side != 'black':
+        return 'you can only be \'white\' or \'black\' because this is chess OMEGALUL'
+    elif author is None:
+        return 'A unique ID for the author must be provided!'
+    elif is_pvp and invitee is None:
+        return 'A unique ID for the invitee/challenged must be provided!'
+    
+    return None
 
 # make a move for the player
 @app.route('/move',methods = ['POST', 'GET'])
 def move():
     args = utils.parse_args(request)
-    is_pvp = 'opponent' in args
+    mover = utils.get_first_valid_index(args, ['self'])
+    opponent = utils.get_first_valid_index(args, ['opponent'])
+    move_intent = utils.get_first_valid_index(args, ['move'])
+
+    if opponent is None:
+        return move_most_recent(args, mover, move_intent)
+    elif opponent == "!":
+        return move_ai(args, mover, move_intent)
+    else:
+        return move_pvp(args, mover, opponent, move_intent)
+
+def move_most_recent(args, mover, move_intent):
+    pass
+
+def move_ai(args, mover, move_intent):
     db = db_session()
     # check to make sure player is in a game
-    curr_game = None
-    if is_pvp:
-        curr_game = utils.check_in_pvp(args['uid'], args['opponent'], db)
-    else:
-        curr_game = utils.check_in_game(args['uid'], db)
-    if curr_game == None:
-        pvp_addon = f' with {utils.mention_player(args["opponent"])}' if is_pvp else ''
-        return f'bruh ur not in a game rn {utils.mention(args)}{pvp_addon}'
-    # if pvp, ensure that it is the player's turn
-    if is_pvp:
-        player_num = curr_game.stockfish_elo
-        if args['uid'] != curr_game.uid.split(',')[player_num]:
-            return f'it not ur turn lol?? {utils.mention(args)}'
-    # load the game in stockfish and verify that the move is legal
-    if not utils.check_move(stockfish, curr_game.moves, args['move']):
+    game = utils.check_in_game(mover, db)
+
+    if game is None:
+        # pvp_addon = f' with {utils.mention_player(args["opponent"])}' if is_pvp else ''
+        return f'bruh ur not in a solo game rn {utils.mention(args)}'
+    
+    if not utils.check_move(stockfish, game, move_intent):
         return f'u can\'t play that lol {utils.mention(args)}'
-    # if move is legal, add the move (and switch player turn if pvp)
-    new_moves = curr_game.moves + ' ' + args['move']
-    if is_pvp:
-        curr_game.stockfish_elo = 1-curr_game.stockfish_elo
+    
+    best = utils.engine_move(stockfish, game)
+
     # did player win?
-    gameover_text = utils.get_gameover_text(stockfish, new_moves, player_just_moved=True)
-    logger.debug(f"PLAYER MOVE GAMEOVER?: {gameover_text}")
-    if gameover_text != '':
+    gameover_text = utils.get_gameover_text(stockfish, game)
+    app.logger.debug(f"PLAYER MOVE GAMEOVER?: {gameover_text}")
+
+    if gameover_text is not None:
         # end game
-        logger.debug("PLAYER WIN!")
-        db.delete(curr_game)
+        app.logger.debug("PLAYER WIN!")
+        utils.complete_game(game)
         return gameover_text
-    # make engine move if vs cpu
-    if not is_pvp:
-        best = utils.engine_move(stockfish, new_moves, curr_game.stockfish_elo)
-        new_moves = new_moves + ' ' + best
-    curr_game.moves = new_moves
-    # did engine/person win?
-    gameover_text = utils.get_gameover_text(stockfish, new_moves, player_just_moved=False)
-    logger.debug(f"CPU MOVE GAMEOVER?: {gameover_text}")
+
+    gameover_text = utils.get_gameover_text(stockfish, game)
+    app.logger.debug(f"CPU MOVE GAMEOVER?: {gameover_text}")
     if gameover_text != '':
         # end game
-        logger.debug("CPU WIN!")
-        db.delete(curr_game)
+        app.logger.debug("CPU WIN!")
+        utils.complete_game(game)
         return utils.relay_move(best, args) + '\n' + gameover_text
     db.commit()
-    if is_pvp:
-        return 'ur move has been made good job pogO'
+
     return utils.relay_move(best, args)
+
+
+def move_pvp(args, mover, opponent, move_intent):
+    db = db_session()
+    game = utils.check_in_game(mover, db)
+
+    if game is None:
+        # pvp_addon = f'  ''
+        result = f'bruh ur not in a solo game rn {utils.mention(args)}'
+        + f'with {utils.mention_player(args["opponent"])}'
+        
+        return result
+
+    if not utils.check_movers_turn(game, mover, db):
+        return f'it not ur turn lol?? {utils.mention(args)}'
+    
+    # load the game in stockfish and verify that the move is legal
+    if not utils.check_move(stockfish, game, move_intent):
+        return f'u can\'t play that lol {utils.mention(args)}'
+
+    # if move is legal, add the move (and switch player turn if pvp)
+    if not utils.make_move(game, mover, move_intent):
+        return f'Something Went Wrong in Making that Move (Are u hecking bro?)'
+    
+    # did player win?
+    gameover_text = utils.get_gameover_text(stockfish, game)
+    app.logger.debug(f"PLAYER MOVE GAMEOVER?: {gameover_text}")
+
+    if gameover_text is not None:
+        # end game
+        app.logger.debug("PLAYER WIN!")
+        utils.complete_game(game)
+        return gameover_text
+        
+    # did engine/person win?
+    return f'ur move has been made good job pogO {utils.mention(args)}'
+    
 
 # accept the player's resignation
 @app.route('/ff',methods = ['POST', 'GET'])
 def ff():
     args = utils.parse_args(request)
-    is_pvp = 'opponent' in args
     db = db_session()
-    # check to make sure player is in a game
-    curr_game = None
-    if is_pvp:
-        curr_game = utils.check_in_pvp(args['uid'], args['opponent'], db)
-    else:
-        curr_game = utils.check_in_game(args['uid'], db)
-    if curr_game == None:
-        return 'bruh ur not in a game rn' + utils.mention(args)
+    mover = utils.get_first_valid_index(args, ['self'])
+    opponent = utils.get_first_valid_index(args, ['opponent'])
+
+    is_pvp, game = get_game(db, args, mover, opponent)
+
+    if game == None:
+        return 'bruh don\'t know what game you speak of' + utils.mention(mover)
+
     # resign the player and claim victory
-    db.delete(curr_game)
-    db.commit()
-    if not is_pvp:
-        return utils.claim_victory(stockfish, curr_game.moves, args)
+    utils.complete_game(game)
+
+    if is_pvp:
+        return utils.claim_victory(stockfish, game)
     else:
-        return utils.pvp_claim_victory(stockfish, curr_game.moves, args)
+        return utils.claim_victory(stockfish, game)
 
 # allow the player to cheat
 @app.route('/cheat',methods = ['POST', 'GET'])
 def cheat():
     args = utils.parse_args(request)
-    is_pvp = 'opponent' in args
     db = db_session()
-    # check to make sure player is in a game
-    curr_game = None
-    if is_pvp:
-        curr_game = utils.check_in_pvp(args['uid'], args['opponent'], db)
-    else:
-        curr_game = utils.check_in_game(args['uid'], db)
-    if curr_game == None:
-        return 'bruh ur not in a game rn' + utils.mention(args)
+    mover = utils.get_first_valid_index(args, ['self'])
+    opponent = utils.get_first_valid_index(args, ['opponent'])
+
+    _, game = get_game(db, args, mover, opponent)
+
+    if game == None:
+        return 'bruh don\'t know what game you speak of' + utils.mention(mover)
+
     # return cheats
-    board, eval, best = utils.cheat_board(stockfish, curr_game.moves)
+    board, eval, best = utils.cheat_board(stockfish, game)
 
     return board + '\n' + eval + '\n' + best + '\n' + 'that good enough for you? stupid cheater' + utils.mention(args)
+
+def get_game(db, args, mover, opponent):
+    is_pvp = False
+    game = None
+    if opponent is None:
+        game, is_pvp = utils.get_most_recent_game(db, args, mover)
+    elif opponent == "!":
+        is_pvp = True
+        game = utils.get_ai_game(db, args, mover)
+    else:
+        is_pvp = False
+        game = utils.get_pvp_game(db, args, mover)
+    
+    return is_pvp, game
 
 if __name__ == '__main__':
    app.run(debug = True)
--- api/db/database.py
+++ api/db/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.sql import func as sqlfunc
+from sqlalchemy.sql import text
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer, Boolean, TIMESTAMP
+from sqlalchemy import Column, String, Integer, Boolean, TIMESTAMP, ForeignKeyConstraint
 
 Base = declarative_base()
 Session = None
@@ -27,22 +28,63 @@ def db_session():
 
     return Session()
 
+## DEPRECATED
 class GameLegacy(Base):
     __tablename__ = 'games'
 
+    # BUG primary keys are automatically unique in most type of SQL.
     uid = Column(String, primary_key=True, unique=True)
     moves = Column(String)
     stockfish_elo = Column(Integer)
     player_side = Column(String)
 
+class Participant(Base):
+    __tablename__ = 'participant'
+
+    id=Column(Integer, primary_key=True, autoincrement=True)
+    discord_user_id=Column(String, unique=True)
+    dicord_guild_id=Column(String)
+    last_updated = Column(TIMESTAMP, nullable=False, server_default=sqlfunc.current_timestamp(), server_onupdate=text(f' ON UPDATE {sqlfunc.current_timestamp()}'))
+
 class StockfishGame(Base):
     __tablename__ = 'stockfish_game'
 
     id=Column(Integer, primary_key=True, autoincrement=True)
-    participant_id = Column(unique=True)
-    moves_id=Column(Integer)
-    next_move=Column(Boolean)
+    participant_id = Column(Integer, nullable=False, unique=True)
     stockfish_elo = Column(Integer)
-    player_side = Column(String)
-    last_updated=Column(TIMESTAMP, nullable=False, server_default=sqlfunc.current_timestamp())
-    
+    player_is_white = Column(Boolean, nullable=False)
+    finished = Column(Boolean, nullable=False, default=False)
+    last_updated=Column(TIMESTAMP, nullable=False, server_default=sqlfunc.current_timestamp(), server_onupdate=text(f' ON UPDATE {sqlfunc.current_timestamp()}'))
+    ForeignKeyConstraint(['participant_id'], ['Participant.id'], name='FK_Participant_Stockfish')
+
+class StockfishMoves(Base):
+    __tablename__ = 'stockfish_moves'
+
+    id=Column(Integer, primary_key=True, autoincrement=True)
+    game_id=Column(Integer)
+    move=Column(String, nullable=False)
+    white_move=Column(Boolean, nullable=False)
+    last_updated = Column(TIMESTAMP, nullable=False, server_default=sqlfunc.current_timestamp(), server_onupdate=text(f' ON UPDATE {sqlfunc.current_timestamp()}'))
+    ForeignKeyConstraint(['game_id'], ['StockfishGame.id'], name='FK_MOVE_STOCKFISH')
+
+class CompetitiveGame(Base):
+    __tablename__ = 'pvp_game'
+
+    id=Column(Integer, primary_key=True, autoincrement=True)
+    author_id = Column(Integer, nullable=False)
+    invitee_id = Column(Integer, nullable=False)
+    author_is_white = Column(Boolean, nullable=False)
+    finished = Column(Boolean, nullable=False, default=False)
+    last_updated = Column(TIMESTAMP, nullable=False, server_default=sqlfunc.current_timestamp(), server_onupdate=text(f' ON UPDATE {sqlfunc.current_timestamp()}'))
+    ForeignKeyConstraint(['author_id'], ['Participant.id'], name='FK_AUTHOR_PVP')
+    ForeignKeyConstraint(['invitee_id'], ['Participant.id'], name='FK_INVITEE_PVP')
+
+class CompetitiveMoves(Base):
+    __tablename__ = 'pvp_moves'
+
+    id=Column(Integer, primary_key=True, autoincrement=True)
+    game_id=Column(Integer)
+    move=Column(String, nullable=False)
+    white_move=Column(Boolean, nullable=False)
+    last_updated = Column(TIMESTAMP, nullable=False, server_default=sqlfunc.current_timestamp(), server_onupdate=text(f' ON UPDATE {sqlfunc.current_timestamp()}'))
+    ForeignKeyConstraint(['game_id'], ['CompetitiveGame.id'], name='FK_MOVE_PVP')
