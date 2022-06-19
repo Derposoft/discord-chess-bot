b27ec23183961b998d341b3289c172c2fa058d49
TFlexSoom <tchilbert@live.com>
2022-06-19 13:10:27 -0700

    Add __init__ file to bot lib directory. Remove dummy Log and replace with NullHandler

--- bot/lib/dummylog.py
+++
@@ -1,8 +0,0 @@
-class dummylog:
-    def __init__(self):
-        pass
-
-    # TODO it may be better to make this a kwargs signature to really
-    #   not care about what is passed to this object
-    def debug(self, msg):
-        pass
